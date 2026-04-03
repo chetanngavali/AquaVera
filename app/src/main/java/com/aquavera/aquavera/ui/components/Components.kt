ffb8ec66f540ffae9b224f81ef3417bcafaa38a2
chetanngavali <chetanngavali@gmail.com>
2026-04-03 14:58:52 +0530

    docs: Purge non-documentation files from repository

--- app/src/main/java/com/aquavera/aquavera/ui/components/Components.kt
+++
@@ -1,3 +0,0 @@
-package com.aquavera.aquavera.ui.components
-
-// All components have been moved to individual files.
--- app/src/main/new.sh
+++
@@ -1,3 +0,0 @@
-sh
-git add .
-git commit -m "Initial commit - AquaVera App with Supabase and EmailJS"
